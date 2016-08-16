c513bdbc5b97bb1cd2472e090933c8430e011288
Ryan <ryan.o.mackey@gmail.com>
2016-08-16 16:24:28 -0600

    added some styling

--- www/js/services.js
+++ www/js/services.js
@@ -5,25 +5,25 @@ angular.module('KitApp.services', [])
 .service('LoginService', ['$http', '$window', function($http, $window) {
   var vm = this;
 
-  vm.showLogin = {show:true};
+  vm.loginView = {show:true};
 
   vm.login = function(username, password) {
     $http.post('http://localhost:3000/auth/login', {username: username, password: password})
     .then(function(response) {
       console.log(response);
       $window.sessionStorage.token = response.data.token;
-      vm.showLogin.show = false;
+      vm.loginView.show = false;
     })
     .catch(function(err) {
       console.log(err);
       delete $window.sessionStorage.token;
-      vm.showLogin.show = true;
+      vm.loginView.show = true;
     });
   };
 
   vm.logout = function() {
     delete $window.sessionStorage.token;
-    vm.showLogin.show = true;
+    vm.loginView.show = true;
   };
 
 }])
