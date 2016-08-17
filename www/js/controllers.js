bffa6a7f00c81c66f214439692f04df253358955
Charles Stafford <cstaff@Charless-MacBook-Pro.local>
2016-08-17 14:30:36 -0600

    pulling add contact

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -40,6 +40,8 @@ angular.module('KitApp.controllers', [])
 
   vm.message = ContactService.message;
 
+
+
   vm.showAddContactForm = false;
   var isIOS = ionic.Platform.isIOS();
   //Works
@@ -47,38 +49,45 @@ angular.module('KitApp.controllers', [])
     $cordovaContacts.pickContact()
      .then(function(result) {
 
-       if(isIOS == true){
-         alert(Object.keys(result));
+       if(isIOS === true){
+        //  alert(Object.keys(result));
         //  alert(result.name.givenName);
         //  alert(result.name.familyName);
          alert(result.name.formatted); // USE THIS FOR SIMPLICITY
-         alert(Object.keys(result.phoneNumbers)); //This is an array // [object, Object]
-         alert(JSON.stringify(result.phoneNumbers[0])); //This is an array // [object, Object]
-         alert(result.phoneNumbers[0].value); // PHONE NUMBER!!
-         alert(result.phoneNumbers[0].type); //PHONE NUMBER TYPE
-         alert(Object.keys(result));
+        //  alert(Object.keys(result.phoneNumbers)); //This is an array // [object, Object]
+        //  alert(JSON.stringify(result.phoneNumbers[0])); //This is an array // [object, Object]
+        //  alert(result.phoneNumbers[0].value); // PHONE NUMBER!!
+        //  alert(result.phoneNumbers[0].type); //PHONE NUMBER TYPE
+        //  alert(JSON.stringify(result.email));
+        //  alert(Object.keys(result));
+
+         ContactService.addContact(result.name.formatted, result.phoneNumbers[0].value);
+         console.log("After addContact");
 
        } else {
          // result // this is an object
-          alert(Object.keys(result));
+          // alert(Object.keys(result));
          //  alert(result.id); //This is number
           // alert(result.displayName); //This is text // N/A in IOS
           // alert(result.name.familyName);
           // alert(result.givenName);
           alert(result.name.formatted); //USE FOR SIMPlICITY
-          // alert(Object.keys(result.name));
-          alert(JSON.stringify(result.phoneNumbers[0])); //This is an array // [object, Object]
-          alert(result.phoneNumbers[0].value); // PHONE NUMBER!!
-          alert(result.phoneNumbers[0].type); //PHONE NUMBER TYPE
+          // // alert(Object.keys(result.name));
+          // alert(JSON.stringify(result.phoneNumbers[0])); //This is an array // [object, Object]
+          // alert(result.phoneNumbers[0].value); // PHONE NUMBER!!
+          // alert(result.phoneNumbers[0].type); //PHONE NUMBER TYPE
+
+          vm.addContact(result.name.formatted, result.phoneNumbers[0].value);
+          console.log("After addContact");
 
        }
 
-       
+       console.log("Inside cordovaContacts");
 
      });
    };
 
-  
+
   // vm.contacts.arr[i].showForm = false;
 
 
