db31b9f838f9a6dc988c3a1744f33961e5b6cfe4
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-08-18 07:23:49 -0700

    signup fully validated and styled

--- www/js/directives.js
+++ www/js/directives.js
@@ -26,6 +26,7 @@ angular.module('KitApp.directives', [])
     templateUrl: 'templates/contact-suggestion.html'
   };
 })
+
 .directive('kaAddContact', function() {
   return {
     restrict: 'E',
@@ -48,6 +49,25 @@ angular.module('KitApp.directives', [])
       scope.$watch("otherModelValue", function() {
         ngModel.$validate();
       });
+    }
+  };
+})
+
+
+.directive('passwordValidation', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, element, attr, mCtrl) {
+      function myValidation(password) {
+        console.log('password validation directive checking validity');
+        if (password.match(/\W/g) && password.match(/\d/g)) {
+            mCtrl.$setValidity('charE', true);
+        } else {
+            mCtrl.$setValidity('charE', false);
+        }
+        return password;
+      }
+      mCtrl.$parsers.push(myValidation);
     }
   };
 });
